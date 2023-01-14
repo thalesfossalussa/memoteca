829c1ec7e1f125705d1c8df7022e6af61c071500
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-14 19:47:20 -0300

    Adicionando projeto de estudo ao github

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Pensamento } from './../pensamento';
+import { Component } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css'],
+})
+export class CriarPensamentoComponent {
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  constructor(private service: PensamentoService, private router: Router) {}
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listarPensamento']);
+  }
+}
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Pensamento } from './../pensamento';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css'],
+})
+export class PensamentoComponent {
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Thales Fossalussa',
+    modelo: 'modelo3',
+  };
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
+}
